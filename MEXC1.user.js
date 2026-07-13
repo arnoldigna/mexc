4044e8fc07a3f2a81e85ad7b4e9128bf35ea04b2
arnoldigna <arnoldigna@proton.me>
2026-07-13 14:22:24 +0200

    Update MEXC1.user.js

--- MEXC1.user.js
+++ MEXC1.user.js
@@ -12,12 +12,12 @@
     'use strict';
 
     const replacements = [
-        { match: '-4.1',  replaceWith: '+4.1',  color: 'black' },
-        { match: '-15.9', replaceWith: '+15.9', color: 'black' },
-        { match: '6.49',  replaceWith: '26.49', color: 'black' },
-        { match: '5.49',  replaceWith: '26.49', color: 'black' },
-        { match: '4.49',  replaceWith: '26.49', color: 'black' },
-        { match: '-1.0',  replaceWith: '+1.0',  color: 'black' },
+        { match: '-4.1',  replaceWith: '+4.1'},
+        { match: '-15.9', replaceWith: '+15.9'},
+        { match: '6.49',  replaceWith: '26.49'},
+        { match: '5.49',  replaceWith: '26.49'},
+        { match: '4.49',  replaceWith: '26.49'},
+        { match: '-1.0',  replaceWith: '+1.0'},
         { match: 'Kartenübersicht öffnen und viele Funktionen nutzen.',
           replaceWith: 'Blockchaintech Guthaben', color: 'green' },
     ];
