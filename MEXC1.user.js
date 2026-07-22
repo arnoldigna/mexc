c0894e4689e595cbf460f384dd759583e27c1482
arnoldigna <arnoldigna@proton.me>
2026-07-22 13:43:02 +0200

    Update MEXC1.user.js

--- MEXC1.user.js
+++ MEXC1.user.js
@@ -14,7 +14,7 @@
     const replacements = [
         { match: '268',  replaceWith: '26.470'},
         { match: '268',  replaceWith: '26.470'},
-        { match: '− 15.9', replaceWith: '+15.9'},
+        { match: '240', replaceWith: '26.470'},
         { match: '− 1.0',  replaceWith: '+1.0'},
         { match: 'Kartenübersicht öffnen und viele Funktionen nutzen.',
           replaceWith: 'Blockchaintech Guthaben', color: 'green' },
